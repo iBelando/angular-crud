a485432ff418b8392658ba571852985b6c85fb76
Glitch2250 <45720803+Glitch2250@users.noreply.github.com>
2020-11-22 00:26:06 -0300

    v2.5

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/angular-crud'));
+app.use(express.static(__dirname+'/angularapp'));
 app.get('/', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/angular-crud/index.html'))
+  res.sendFile(path.join(__dirname+'/angularapp/index.html'))
 });
 
 app.listen(process.env.PORT || 8080);
