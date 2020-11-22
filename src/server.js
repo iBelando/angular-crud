5e3fc150174f807e60b3446014cfac9184ceebc7
Glitch2250 <45720803+Glitch2250@users.noreply.github.com>
2020-11-22 00:06:20 -0300

    v2.0

--- src/server.js
+++ src/server.js
@@ -7,3 +7,5 @@ app.use(express.static(__dirname+'/dist/angular-crud'));
 app.get('/', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/angular-crud/index.html'))
 });
+
+app.listen(process.env.PORT || 8080);
